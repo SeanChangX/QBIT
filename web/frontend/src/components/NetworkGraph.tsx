187afd25ba753569a614e856ed7b390260f34a79
SeanChangX <scx@gapp.nthu.edu.tw>
2026-02-18 18:25:28 +0800

    fix: remove unused drag and zoom options from NetworkGraph component to streamline interaction

--- web/frontend/src/components/NetworkGraph.tsx
+++ web/frontend/src/components/NetworkGraph.tsx
@@ -113,27 +113,9 @@ export default function NetworkGraph({
           tooltipDelay: 200,
           selectable: true,
           dragNodes: true,
-          dragView: true,
-          zoomView: true,
         },
       }
     );
-
-    const net = networkRef.current;
-
-    // When user pans the view (drag canvas) or zooms: run physics so graph "floats" then settles
-    const triggerFloatThenSettle = (): void => {
-      net.setOptions({ physics: { enabled: true } });
-      net.stabilize(100);
-    };
-
-    net.on('dragEnd', (params: { nodes: string[] }) => {
-      if (params.nodes.length === 0) triggerFloatThenSettle();
-    });
-    net.on('zoom', () => triggerFloatThenSettle());
-    net.on('stabilized', () => {
-      net.setOptions({ physics: { enabled: false } });
-    });
 
     // Click handler -- open poke dialog for device nodes or user poke for user nodes (self not clickable)
     networkRef.current.on('click', (params: { nodes: string[] }) => {
